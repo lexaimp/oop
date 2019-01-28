99207ab2cf00b1e0f904d3a2694e711a3490dbc5
Alexey <lexaimp1993rus@yandex.ru>
2019-01-28 22:52:18 +0700

    Range complete

--- Range/src/ru/academIT/babushkin/Main/Main.java
+++ Range/src/ru/academIT/babushkin/Main/Main.java
@@ -14,25 +14,32 @@ public class Main {
         System.out.println(segment.getLength());
         System.out.println(segment.isInside(14));
 
+        System.out.println("Введите данные первого интервала:");
         Range range1 = new Range(scanner.nextDouble(), scanner.nextDouble());
+        System.out.println("Введите данные второго интервала:");
         Range range2 = new Range(scanner.nextDouble(), scanner.nextDouble());
+
+        System.out.println("Пересечение:");
+        System.out.print("Интервал пересечения двух интервалов: ");
         Range range3 = range1.getIntersection(range2);
         if (range3 == null) {
             System.out.println("null");
         } else {
-            System.out.println(range3.getFrom() + " " + range3.getTo());
+            System.out.println("(" + range3.getFrom() + "; " + range3.getTo() + ")");
         }
+        System.out.println("Объединение:");
         Range[] range4 = range1.getUnion(range2);
         if (range4.length == 2) {
-            System.out.println(range4[0].getFrom() + " " + range4[0].getTo() + " " + range4[1].getFrom() + " " + range4[1].getTo());
+            System.out.println("Получилось 2 интервала объединения: 1 интервал: (" + range4[0].getFrom() + "; " + range4[0].getTo() + ") и 2 интервал: (" + range4[1].getFrom() + "; " + range4[1].getTo() + ")");
         } else {
-            System.out.println(range4[0].getFrom() + " " + range4[0].getTo());
+            System.out.println("Интервал объединения двух интервалов: (" + range4[0].getFrom() + "; " + range4[0].getTo() + ")");
         }
+        System.out.println("Разность:");
         range4 = range1.getDifference(range2);
         if (range4.length == 2) {
-            System.out.println(range4[0].getFrom() + " " + range4[0].getTo() + " " + range4[1].getFrom() + " " + range4[1].getTo());
+            System.out.println("Получилось 2 интервала разности: 1 интервал: (" + range4[0].getFrom() + "; " + range4[0].getTo() + ") и 2 интервал: (" + range4[1].getFrom() + "; " + range4[1].getTo() + ")");
         } else if (range4.length == 1) {
-            System.out.println(range4[0].getFrom() + " " + range4[0].getTo());
+            System.out.println("Интервал разности двух интервалов: (" + range4[0].getFrom() + "; " + range4[0].getTo() + ")");
         } else {
             System.out.println("Пустое множество");
         }
