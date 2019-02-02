fc55952cc967a968e2af42f63d49ce970563d83e
Alexey <lexaimp1993rus@yandex.ru>
2019-02-03 01:22:55 +0700

    add 2 comparator

---
+++ Shapes/src/ru/academIT/babushkin/Main/AreaComparator.java
@@ -0,0 +1,12 @@
+package ru.academIT.babushkin.Main;
+
+import ru.academIT.babushkin.Shapes.Shape;
+
+import java.util.Comparator;
+
+public class AreaComparator implements Comparator<Shape> {
+    @Override
+    public int compare(Shape shape1, Shape shape2){
+        return Double.compare(shape1.getArea(), shape2.getArea());
+    }
+}
--- Shapes/src/ru/academIT/babushkin/Main/Main.java
+++ Shapes/src/ru/academIT/babushkin/Main/Main.java
@@ -2,9 +2,18 @@ package ru.academIT.babushkin.Main;
 
 import ru.academIT.babushkin.Shapes.*;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(13, 78, 874, -87, 98, 7812);
-        System.out.println(triangle.getArea());
+        Shape triangle1 = new Triangle(1, 7, 15, 3, 2, 6);
+        Shape rectangle1 = new Rectangle(5, 3);
+        Shape rectangle2 = new Rectangle(2, 7.3);
+        Shape shapes[] = {new Square(1), new Circle(23.3), new Circle(5.55)};
+        System.out.println(Arrays.toString(shapes));
+        Arrays.sort(shapes, new AreaComparator());
+        System.out.println(Arrays.toString(shapes));
+        Arrays.sort(shapes, new PerimeterComparator());
+        System.out.println(Arrays.toString(shapes));
     }
 }
