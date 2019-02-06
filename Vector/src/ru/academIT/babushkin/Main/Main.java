368d1e3e858ec9b7d72d1defc61dd4f5d2a59b04
Alexey <lexaimp1993rus@yandex.ru>
2019-02-07 00:20:01 +0700

    created constructor

--- Vector/src/ru/academIT/babushkin/Main/Main.java
+++ Vector/src/ru/academIT/babushkin/Main/Main.java
@@ -4,7 +4,15 @@ import ru.academIT.babushkin.Vector.*;
 
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector(2);
-        System.out.println();
+        Vector vector = new Vector(5);
+        System.out.println(vector.toString());
+        Vector vector1 = new Vector(vector);
+        System.out.println(vector1.toString());
+        double[] array = {1.2, 15.4, 18.23, 1.1, 45};
+        Vector vector2 = new Vector(array);
+        System.out.println(vector2.toString());
+        Vector vector3 = new Vector(10, array);
+        System.out.println(vector3.toString());
+        System.out.println(vector2.getSize());
     }
 }
--- Vector/src/ru/academIT/babushkin/Vector/Vector.java
+++ Vector/src/ru/academIT/babushkin/Vector/Vector.java
@@ -1,28 +1,56 @@
 package ru.academIT.babushkin.Vector;
 
+import java.util.Arrays;
+
 public class Vector {
-    private int n;
     private double[] array;
 
     public Vector(int n) {
         if (n <= 0) {
             throw new IllegalArgumentException();
         }
-        this.n = n;
         this.array = new double[n];
-        for (int i = 0; i == n; i++) {
-            this.array[i] = 0;
-        }
     }
 
-    public Vector(Vector vector){
-        this.n = vector.n;
+    public Vector(Vector vector) {
         this.array = vector.array;
     }
 
-    public Vector ()
-    public Vector(int n, double[] array) {
-        this.n = n;
+    public Vector(double[] array) {
         this.array = array;
+    }
+
+    public Vector(int n, double[] array) {
+        this.array = Arrays.copyOf(array, n);
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+    @Override
+    public String toString() {
+        if (array == null) {
+            return "null";
+        } else {
+            int var1 = array.length - 1;
+            if (var1 == -1) {
+                return "{}";
+            } else {
+                StringBuilder var2 = new StringBuilder();
+                var2.append("{");
+                int var3 = 0;
+
+                while (true) {
+                    var2.append(array[var3]);
+                    if (var3 == var1) {
+                        return var2.append("}").toString();
+                    }
+
+                    var2.append(", ");
+                    ++var3;
+                }
+            }
+        }
     }
 }
