c811aa726b842227d8a1eb74c9a70ff988360611
lexaimp <a.babushkin.rosdomofon.com>
2019-02-07 20:52:08 +0700

    work

--- Vector/src/ru/academIT/babushkin/Vector/Vector.java
+++ Vector/src/ru/academIT/babushkin/Vector/Vector.java
@@ -13,7 +13,8 @@ public class Vector {
     }
 
     public Vector(Vector vector) {
-        this.array = vector.array;
+
+        this.array = Arrays.copyOf(vector.array, vector.array.length);
     }
 
     public Vector(double[] array) {
@@ -45,7 +46,20 @@ public class Vector {
         }
     }
 
-   /* public static Vector getSum (Vector vector1, Vector vector2) {
-        vector1.array
-    }*/
+    public Vector getMin(Vector vector1, Vector vector2) {
+        return vector1.getSize() <= vector2.getSize() ? vector1 : vector2;
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        if (vector1.getSize() >= vector2.getSize()) {
+            for (int i = 0; i < vector2.getSize(); i++) {
+                vector1.array[i] += vector2.array[i];
+            }else{
+                for (int i = 0; i < vector1.getSize(); i++) {
+                    vector2.array[i] += vector1.array[i];
+                }
+            }
+            return vector1;
+        }
+    }
 }
