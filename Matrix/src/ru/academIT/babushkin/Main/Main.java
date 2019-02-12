1cfcf1ec4ffa0a9f7530fe954afe722ffc497f88
lexaimp <a.babushkin.rosdomofon.com>
2019-02-12 18:00:11 +0700

    work

--- Matrix/src/ru/academIT/babushkin/Main/Main.java
+++ Matrix/src/ru/academIT/babushkin/Main/Main.java
@@ -1,8 +1,22 @@
 package ru.academIT.babushkin.Main;
 
 import ru.academIT.babushkin.Matrix.*;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Matrix matrix = new Matrix(5, 5);
+        System.out.println(matrix.toString());
+
+        double[][] array = new double[4][2];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 10);
+            }
+        }
+
+        Matrix matrix1 = new Matrix(array);
+        System.out.println(matrix1.toString());
     }
 }
--- Matrix/src/ru/academIT/babushkin/Matrix/Matrix.java
+++ Matrix/src/ru/academIT/babushkin/Matrix/Matrix.java
@@ -1,13 +1,43 @@
 package ru.academIT.babushkin.Matrix;
 
 import ru.academIT.babushkin.Vector.*;
+
+import java.util.Arrays;
+
 public class Matrix {
     private Vector[] matrix;
 
-    public Matrix (int n, int m){
+    public Matrix(int n, int m) {
         matrix = new Vector[m];
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             matrix[i] = new Vector(n);
+        }
+    }
+
+    public Matrix(Matrix matrix) {
+        this.matrix = Arrays.copyOf(matrix.matrix, matrix.matrix.length);
+    }
+
+    public Matrix(double[][] array) {
+        for(int i = 0; i < array.length; i++) {
+            matrix[i] = new Vector(array[i].length, array[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{");
+        int var = 0;
+
+        while (true) {
+            stringBuilder.append(matrix[var].toString());
+            if (var == matrix.length - 1) {
+                return stringBuilder.append("}").toString();
+            }
+
+            stringBuilder.append(", ");
+            ++var;
         }
     }
 }
