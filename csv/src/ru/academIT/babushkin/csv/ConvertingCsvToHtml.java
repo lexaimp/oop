e642284a8ab0b5c432a2e5934b5642699521bf71
Alexey <lexaimp1993rus@yandex.ru>
2019-02-26 00:46:43 +0700

    csv created

---
+++ csv/src/ru/academIT/babushkin/csv/ConvertingCsvToHtml.java
@@ -0,0 +1,36 @@
+package ru.academIT.babushkin.csv;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ConvertingCsvToHtml {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(new FileReader("csv/src/ru/academIT/babushkin/csv/example.csv")); FileWriter fileWriter = new FileWriter("csv/src/ru/academIT/babushkin/csv/example.html")) {
+            fileWriter.append("<!DOCTYPE html>\n")
+                    .append("<html>\n")
+                    .append("<head>\n")
+                    .append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n")
+                    .append("<title>example</title>\n")
+                    .append("</head>\n")
+                    .append("<body>\n")
+                    .append("<h1>Таблица</h1>\n")
+                    .append("<table border=\"1\">\n");
+
+            while (scanner.hasNext()) {
+                fileWriter.append(scanner.nextLine());
+            }
+
+            fileWriter.append("\n</table>\n")
+                    .append(("</body>\n"))
+                    .append("</html>");
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
+/*
+<tr><th>текст заголовка</th><th>текст заголовка</th></tr> <!--ряд с ячейками заголовков-->
+<tr><td>данные</td><td>данные</td></tr> <!--ряд с ячейками тела таблицы-->
+</table>*/
