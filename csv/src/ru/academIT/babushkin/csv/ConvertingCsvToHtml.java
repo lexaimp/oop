04b71a8ead041004df5b666035b1dae3f4d734da
Alexey <lexaimp1993rus@yandex.ru>
2019-03-26 23:46:27 +0700

    made corrections to ConvertingCsvToHtml

--- csv/src/ru/academIT/babushkin/csv/ConvertingCsvToHtml.java
+++ csv/src/ru/academIT/babushkin/csv/ConvertingCsvToHtml.java
@@ -8,79 +8,79 @@ import java.util.Scanner;
 public class ConvertingCsvToHtml {
     public static void main(String[] args) {
         if (args.length == 1) {
-            if (args[0].equals("--help")) {
-                System.out.println(getHelp());
-            }
-            if (args[0].equals("--version")) {
-                System.out.println(getVersion());
+            switch (args[0]) {
+                case "--help":
+                    System.out.println(getHelp());
+                    break;
+                case "--version":
+                    System.out.println(getVersion());
+                    break;
             }
             return;
         }
-        if (args.length == 2) {
-            try (Scanner scanner = new Scanner(new FileReader(args[0]));
-                 PrintWriter printWriter = new PrintWriter(args[1])) {
-                String ls = System.lineSeparator();
-                printWriter.print("<!DOCTYPE html>\n" +
-                        "<html>" + ls +
-                        "<head>" + ls +
-                        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" + ls +
-                        "<title>example</title>" + ls +
-                        "</head>" + ls +
-                        "<body>" + ls +
-                        "<h1>Таблица</h1>" + ls +
-                        "<table border=\"1\">");
-
-                boolean cellInQuotes = false;
-                while (scanner.hasNext()) {
-                    if (!cellInQuotes) {
-                        printWriter.print("<tr><td>");
-                    }
-                    String string = scanner.nextLine();
-                    for (int i = 0; i < string.length(); i++) {
-                        char symbol = string.charAt(i);
-
-                        if (symbol == '"') {
-                            if (i == string.length() - 1) {
-                                cellInQuotes = !cellInQuotes;
-                            } else if (string.charAt(i + 1) == '"' && cellInQuotes) {
-                                printWriter.print(symbol);
-                                ++i;
-                            } else {
-                                cellInQuotes = !cellInQuotes;
-                            }
-                        } else if (symbol == ',') {
-                            if (cellInQuotes) {
-                                printWriter.print(",");
-                            } else {
-                                printWriter.print("</td><td>");
-                            }
-                        } else if (symbol == '<') {
-                            printWriter.print("&lt");
-                        } else if (symbol == '>') {
-                            printWriter.print("&gt");
-                        } else if (symbol == '&') {
-                            printWriter.print("&amp");
-                        } else {
-                            printWriter.print(symbol);
-                        }
-
-                    }
-                    if (cellInQuotes) {
-                        printWriter.print("<br/>");
-                    } else {
-                        printWriter.print("</td></tr>");
-                    }
-                }
-                printWriter.println("</table>" + ls +
-                        "</body>" + ls +
-                        "</html>");
-            } catch (FileNotFoundException notFound) {
-                System.out.println("Не удалось найти файл");
-                System.out.println(getHelp());
-            }
-        } else {
+        if (args.length != 2) {
             System.out.println("Передано неверное количество аргументов");
             System.out.println(getHelp());
+        }
+        try (Scanner scanner = new Scanner(new FileReader(args[0]));
+             PrintWriter printWriter = new PrintWriter(args[1])) {
+            String ls = System.lineSeparator();
+            printWriter.print("<!DOCTYPE html>" + ls +
+                    "<html>" + ls +
+                    "<head>" + ls +
+                    "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" + ls +
+                    "<title>example</title>" + ls +
+                    "</head>" + ls +
+                    "<body>" + ls +
+                    "<h1>Таблица</h1>" + ls +
+                    "<table border=\"1\">");
+
+            boolean cellInQuotes = false;
+            while (scanner.hasNext()) {
+                if (!cellInQuotes) {
+                    printWriter.print("<tr><td>");
+                }
+                String string = scanner.nextLine();
+                for (int i = 0; i < string.length(); i++) {
+                    char symbol = string.charAt(i);
+
+                    if (symbol == '"') {
+                        if (i == string.length() - 1) {
+                            cellInQuotes = !cellInQuotes;
+                        } else if (string.charAt(i + 1) == '"' && cellInQuotes) {
+                            printWriter.print(symbol);
+                            ++i;
+                        } else {
+                            cellInQuotes = !cellInQuotes;
+                        }
+                    } else if (symbol == ',') {
+                        if (cellInQuotes) {
+                            printWriter.print(",");
+                        } else {
+                            printWriter.print("</td><td>");
+                        }
+                    } else if (symbol == '<') {
+                        printWriter.print("&lt;");
+                    } else if (symbol == '>') {
+                        printWriter.print("&gt;");
+                    } else if (symbol == '&') {
+                        printWriter.print("&amp;");
+                    } else {
+                        printWriter.print(symbol);
+                    }
+
+                }
+                if (cellInQuotes) {
+                    printWriter.print("<br/>");
+                } else {
+                    printWriter.print("</td></tr>");
+                }
+            }
+            printWriter.println("</table>" + ls +
+                    "</body>" + ls +
+                    "</html>");
+        } catch (FileNotFoundException notFound) {
+            System.out.println("Не удалось найти файл");
         }
     }
 
