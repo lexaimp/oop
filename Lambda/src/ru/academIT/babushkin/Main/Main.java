740ae45d8ecbd16f1ba751e70fa93628fc58cdfb
Alexey <lexaimp1993rus@yandex.ru>
2019-05-19 17:36:59 +0700

    Lambda is ready to check

---
+++ Lambda/src/ru/academIT/babushkin/Main/Main.java
@@ -0,0 +1,42 @@
+package ru.academIT.babushkin.Main;
+
+import ru.academIT.babushkin.Lambda.Person;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Person> peoples = new ArrayList<>();
+        peoples.add(new Person("Коля", 30));
+        peoples.add(new Person("Оля", 30));
+        peoples.add(new Person("Коля", 72));
+        peoples.add(new Person("Петя", 10));
+        peoples.add(new Person("Максим", 23));
+        peoples.add(new Person("Светлана", 17));
+
+        String names = peoples.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.joining(", ", "Имена: ", "."));
+        System.out.println(names);
+
+        OptionalDouble averageAge = peoples.stream()
+                .mapToInt(Person::getAge)
+                .filter(x -> x < 18)
+                .average();
+        System.out.println(averageAge);
+
+        Map<String, Double> map = peoples.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
+        System.out.println(map.toString());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        peoples.stream().filter(x -> x.getAge() >= 20 && x.getAge() <= 45)
+                .sorted(Comparator.comparing(Person::getAge).reversed())
+                .forEachOrdered(person -> stringBuilder.append(person.getName()).append(", "));
+        stringBuilder.setLength(stringBuilder.length() - 2);
+        System.out.println(stringBuilder);
+    }
+}
--- SandBox/src/Main.java
+++ SandBox/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-
+        Predicate<Integer> s = x -> x > 0;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(-1);
+        arrayList.add(14);
+        System.out.println(arrayList);
+        System.out.println(arrayList.stream().filter(s).count());
     }
 }
