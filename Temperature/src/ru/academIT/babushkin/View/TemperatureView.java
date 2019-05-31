47cab98de393dc272cf5033a53116294e20caab0
lexaimp <a.babushkin.rosdomofon.com>
2019-05-31 17:46:43 +0700

    work

--- Temperature/src/ru/academIT/babushkin/View/TemperatureView.java
+++ Temperature/src/ru/academIT/babushkin/View/TemperatureView.java
@@ -5,29 +5,51 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TemperatureView extends JFrame {
+    private String[] temperatureUnits; // Массив для хранения наименования едениц измерения
+
+    private JLabel errorLabel = error();
 
     public TemperatureView(String title) {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle(title);
-        setSize(400, 100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        int width = 500;
+        int height = 180;
+        setSize(width, height);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(screenSize.width / 2 - width / 2, screenSize.height / 2 - height / 2);
         add(panel(), BorderLayout.LINE_START);
-        add(label(), BorderLayout.CENTER);
+        add(errorLabel, BorderLayout.NORTH);
+        add(panelWithButton(), BorderLayout.CENTER);
         add(panel(), BorderLayout.LINE_END);
-        add(panel(), BorderLayout.PAGE_END);
     }
 
     private GridBagConstraints getGridBagConstraints() {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.BOTH;
         constraints.weightx = 1;
+        constraints.insets = new Insets(1, 10, 5, 10);
         constraints.gridwidth = GridBagConstraints.REMAINDER;
         return constraints;
+    }
+
+    private JTextField textFieldForGetInteger() {
+        JTextField field = new JTextField();
+        field.addActionListener(e -> {
+            try {
+                Integer.parseInt(field.getText());
+                errorLabel.setVisible(false);
+            } catch (NumberFormatException exception) {
+                errorLabel.setText("Введите целое число");
+                errorLabel.setVisible(true);
+            }
+        });
+        return field;
     }
 
     private JPanel panel() {
         JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout());
-        panel.add(new JTextField(), getGridBagConstraints());
+        panel.add(textFieldForGetInteger(), getGridBagConstraints());
         String[] items = {
                 "Элемент списка 1",
                 "Элемент списка 2",
@@ -37,16 +59,35 @@ public class TemperatureView extends JFrame {
         return panel;
     }
 
-    private JLabel label() {
-        JLabel label = new JLabel("=");
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        label.setFont(new Font("Font", Font.PLAIN, 40));
-        return label;
+    private JPanel panelWithButton() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        panel.add(new JButton("Перевести"), getGridBagConstraints());
+        return panel;
     }
 
     private JTextField textField() {
         JTextField textField = new JTextField();
         return textField;
+    }
+
+    private JLabel error(String ErrorText, boolean visible) {
+        JLabel label = new JLabel(ErrorText);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setFont(new Font(label.getName(), Font.PLAIN, 15));
+        label.setForeground(Color.RED);
+        label.setVisible(visible);
+        return label;
+    }
+
+    private JLabel error() {
+        JLabel label = new JLabel();
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setFont(new Font(label.getName(), Font.PLAIN, 15));
+        label.setForeground(Color.RED);
+        label.setVisible(false);
+        return label;
     }
 }
