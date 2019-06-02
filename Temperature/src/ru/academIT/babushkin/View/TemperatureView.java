11e9fcf58c51f4389cc363d9640f74432604a182
Alexey <lexaimp1993rus@yandex.ru>
2019-06-03 02:21:50 +0700

    home

--- Temperature/src/ru/academIT/babushkin/View/TemperatureView.java
+++ Temperature/src/ru/academIT/babushkin/View/TemperatureView.java
@@ -9,9 +9,6 @@ import java.awt.*;
 public class TemperatureView extends JFrame {
     private String[] temperatureUnits; // Массив для хранения наименования едениц измерения
 
-    private JLabel errorLabel = error();
-    private JButton button = new JButton("Перевести");
-
     public TemperatureView(String title, String[] temperatureUnits) {
         this.temperatureUnits = temperatureUnits;
         setTitle(title);
@@ -24,23 +21,22 @@ public class TemperatureView extends JFrame {
         setLocation(screenSize.width / 2 - width / 2, screenSize.height / 2 - height / 2);
         setLayout(new GridLayout(1, 3));
 
-        add(panel());
-        add(centralPanel());
-        add(panel());
-    }
-
-    private GridBagConstraints getGridBagConstraints() {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.BOTH;
         constraints.weightx = 1;
         constraints.insets = new Insets(1, 10, 5, 10);
         constraints.gridwidth = GridBagConstraints.REMAINDER;
-        return constraints;
-    }
 
-    private JTextField textFieldForGetInteger() {
-        JTextField field = new JTextField();
-        field.getDocument().addDocumentListener(new DocumentListener() {
+        JTextField textFieldForGetDouble = new JTextField();
+
+        JLabel errorLabel = new JLabel();
+        errorLabel.setFont(new Font(errorLabel.getName(), Font.PLAIN, 15));
+        errorLabel.setForeground(Color.RED);
+        errorLabel.setVisible(false);
+
+        JButton button = new JButton("Перевести");
+
+        textFieldForGetDouble.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
                 IntegerInspection();
@@ -58,44 +54,33 @@ public class TemperatureView extends JFrame {
 
             private void IntegerInspection() {
                 try {
-                    Double.parseDouble(field.getText());
+                    Double.parseDouble(textFieldForGetDouble.getText());
                     errorLabel.setVisible(false);
                     button.setEnabled(true);
                 } catch (NumberFormatException exception) {
-                    errorLabel.setText("Введите число");
+                    errorLabel.setText("Введите числовое значение");
                     errorLabel.setVisible(true);
                     button.setEnabled(false);
 
                 }
             }
         });
-        return field;
-    }
 
-    private JPanel panel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        panel.add(textFieldForGetInteger(), getGridBagConstraints());
-        panel.add(new JComboBox<>(temperatureUnits), getGridBagConstraints());
-        return panel;
-    }
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridBagLayout());
+        inputPanel.add(textFieldForGetDouble, constraints);
+        inputPanel.add(new JComboBox<>(temperatureUnits), constraints);
 
-    private JPanel centralPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 1));
+        JPanel centralPanel = new JPanel();
+        centralPanel.setLayout(new GridLayout(2, 1));
         JPanel panelWithErrorLabel = new JPanel();
         panelWithErrorLabel.setLayout(new GridBagLayout());
-        panelWithErrorLabel.add(errorLabel, getGridBagConstraints());
-        panel.add(panelWithErrorLabel);
-        panel.add(new JPanel().add(button));
-        return panel;
-    }
+        panelWithErrorLabel.add(errorLabel, constraints);
+        centralPanel.add(panelWithErrorLabel);
+        centralPanel.add(new JPanel().add(button));
 
-    private JLabel error() {
-        JLabel label = new JLabel();
-        label.setFont(new Font(label.getName(), Font.PLAIN, 15));
-        label.setForeground(Color.RED);
-        label.setVisible(false);
-        return label;
+        add(inputPanel);
+        add(centralPanel);
+        add(new JPanel());
     }
 }
